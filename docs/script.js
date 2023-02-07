b1f072f751b9493942bc0b6f8106b20be82f2842
CodeByDex <119005046+CodeByDex@users.noreply.github.com>
2023-02-06 20:09:29 -0500

    implemented button click to set static password

--- docs/script.js
+++ docs/script.js
@@ -2,15 +2,19 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+}
+
+function generatePassword(){
+  return "I made a passowrd";
 }
 
 // Add event listener to generate button
